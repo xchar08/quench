24612efe732453bad827a6c0d4dbe9d7a4153f86
xchar08 <108827236+xchar08@users.noreply.github.com>
2025-01-15 11:35:38 -0600

    made monorepo for vercel

--- la-fire-stations-map/backend/server.js
+++ la-fire-stations-map/backend/server.js
@@ -160,7 +160,4 @@ app.post('/api/graphhopper-route', async (req, res) => {
   }
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
